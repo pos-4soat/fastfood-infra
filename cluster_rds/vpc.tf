4ece7005043d842dd9dcc3ec67f750fad4d9553e
Corassin <44197980+Corassin@users.noreply.github.com>
2024-05-02 11:46:07 -0300

    Fix/vpc (#13)
    * delete-vpc-inbound
    
    * rdsTest
    
    * rdsTest-vpc
    
    * fix-vpcid
    
    * removing-apigateway
    
    * rollback

--- cluster_rds/vpc.tf
+++ cluster_rds/vpc.tf
@@ -48,8 +48,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
     egress {
