1612846a5c8067fd7772ac2bfeae9b71d08f521e
Marcello Corassin <marcellocorassin@hotmail.com>
2024-05-03 12:38:00 -0300

    add new ecr

--- ecr/main.tf
+++ ecr/main.tf
@@ -29,6 +29,22 @@ resource "aws_ecr_repository" "ecr_auth" {
   tags = var.tags
 }
 
+resource "aws_ecr_repository" "ecr_order" {
+  name = var.ecr_auth
+
+  image_tag_mutability = var.image_mutability
+
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = var.tags
+}
+
 output "ecr_auth_repository_url" {
   value = aws_ecr_repository.ecr_auth.repository_url
 }
