a168ffaa05769b068952ddec7d5e2b2bb2b3291b
Marcello Corassin <marcellocorassin@hotmail.com>
2024-05-18 12:54:15 -0300

    lambda creation

--- variables.tf
+++ variables.tf
@@ -5,25 +5,25 @@ variable "region" {
 
 variable "create_lambda" {
   type    = bool
-  default = false
+  default = true
 }
 
 variable "products_uri_lb" {
   type    = string
-  default = "http://aeb88d55333df4e3d92d2cbcc667f494-2b39a0f69558a653.elb.us-east-1.amazonaws.com"
+  default = "http://aaf158365ac594b9a935a5728cff86a4-477550370.us-east-1.elb.amazonaws.com"
 }
 
 variable "order_uri_lb" {
   type    = string
-  default = "http://aeb88d55333df4e3d92d2cbcc667f494-2b39a0f69558a653.elb.us-east-1.amazonaws.com"
+  default = "http://afddde2aa9a2349f19ea88a81368d68a-1980996683.us-east-1.elb.amazonaws.com"
 }
 
 variable "payment_uri_lb" {
   type    = string
-  default = "http://aeb88d55333df4e3d92d2cbcc667f494-2b39a0f69558a653.elb.us-east-1.amazonaws.com"
+  default = "http://adbabe667ae49490aace41d0d30d8d2a-58880327.us-east-1.elb.amazonaws.com"
 }
 
 variable "production_uri_lb" {
   type    = string
-  default = "http://aeb88d55333df4e3d92d2cbcc667f494-2b39a0f69558a653.elb.us-east-1.amazonaws.com"
+  default = "http://aba78a0d3d18f421a9719ce666d00116-1778465615.us-east-1.elb.amazonaws.com"
 }
