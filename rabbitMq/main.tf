78758d504a49ef095a42c653e2189ff2c783fa28
Corassin <44197980+Corassin@users.noreply.github.com>
2024-06-17 18:25:43 -0300

    updated resource (#28)

--- rabbitMq/main.tf
+++ rabbitMq/main.tf
@@ -1,10 +1,13 @@
 resource "aws_mq_broker" "rabbitmq" {
-  broker_name        = "my-rabbitmq-broker"
-  engine_type        = "RabbitMQ"
-  engine_version     = "3.12.13"
-  host_instance_type = "mq.t3.micro"
-  security_groups    = [aws_security_group.mq_sg.id]
-  subnet_ids         = var.private_subnets_ids
+  broker_name                   = "my-rabbitmq-broker"
+  deployment_mode               = "ACTIVE_STANDBY_MULTI_AZ"
+  engine_type                   = "RabbitMQ"
+  engine_version                = "3.12.13"
+  host_instance_type            = "mq.t3.micro"
+  auto_minor_version_upgrade    = false
+  apply_immediately             = false
+  security_groups               = [aws_security_group.mq_sg.id]
+  subnet_ids                    = var.private_subnets_ids
   user {
     username = "rabbitUser"
     password = "rabbitPassword"
