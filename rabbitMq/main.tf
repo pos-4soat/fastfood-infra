8743a1ad40973145fa1d9d5e6f7348ecec3d3e92
Marcello Corassin <marcellocorassin@hotmail.com>
2024-06-17 17:00:17 -0300

    broker update

--- rabbitMq/main.tf
+++ rabbitMq/main.tf
@@ -1,12 +1,12 @@
 resource "aws_mq_broker" "rabbitmq" {
   broker_name        = "my-rabbitmq-broker"
   engine_type        = "RabbitMQ"
-  engine_version     = "3.8.11"
-  instance_type      = "mq.t2.micro"
+  engine_version     = "3.12.13"
+  host_instance_type = "mq.t3.micro"
   publicly_accessible = true
   security_groups    = [aws_security_group.mq_sg.id]
   subnet_ids         = var.private_subnets_ids
-  users {
+  user {
     username = "rabbitUser"
     password = "rabbitPwd"
   }
