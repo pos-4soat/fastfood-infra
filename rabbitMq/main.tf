6672897463b79d2572866ec280696fb3c442db41
Marcello Corassin <marcellocorassin@hotmail.com>
2024-06-17 20:03:21 -0300

    instance update

--- rabbitMq/main.tf
+++ rabbitMq/main.tf
@@ -1,9 +1,9 @@
 resource "aws_mq_broker" "rabbitmq" {
   broker_name                   = "my-rabbitmq-broker"
-  deployment_mode               = "SINGLE_INSTANCE"
+  deployment_mode               = "CLUSTER_MULTI_AZ"
   engine_type                   = "RabbitMQ"
   engine_version                = "3.12.13"
-  host_instance_type            = "mq.t3.micro"
+  host_instance_type            = "mq.m5.large"
   auto_minor_version_upgrade    = false
   apply_immediately             = false
   security_groups               = [aws_security_group.mq_sg.id]
