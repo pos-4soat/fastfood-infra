15084488e5eb1af1b6f2b5baedc1e683925a4711
Marcello Corassin <marcellocorassin@hotmail.com>
2024-06-18 17:42:08 -0300

    security group updated

--- rabbitMq/main.tf
+++ rabbitMq/main.tf
@@ -6,7 +6,7 @@ resource "aws_mq_broker" "rabbitmq" {
   host_instance_type            = "mq.m5.large"
   auto_minor_version_upgrade    = false
   apply_immediately             = false
-  security_groups               = var.security_group_id
+  security_groups               = [var.security_group_id]
   subnet_ids                    = var.private_subnets_ids
   user {
     username = "rabbitUser"
