6da607becb8a2629c8154e7b0fa5e644ce7167ab
Corassin <44197980+Corassin@users.noreply.github.com>
2024-06-18 19:40:22 -0300

    added 5672 ingress (#33)
    * added 5672 ingress
    
    * security group id update

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "ecr" {
 
 module "rabbitMq" {
   source = "./rabbitMq"
-
+  
   private_subnets_ids     = module.cluster_rds.private_subnets_ids
   security_group_id       = module.cluster_rds.security_group_id
 }
