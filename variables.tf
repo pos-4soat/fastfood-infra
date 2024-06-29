f67882bbea639d8e176e961cdded8dcbad4779d0
Corassin <44197980+Corassin@users.noreply.github.com>
2024-06-29 12:33:06 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,25 +5,25 @@ variable "region" {
 
 variable "create_lambda" {
   type    = bool
-  default = false
+  default = true
 }
 
 variable "products_uri_lb" {
   type    = string
-  default = "http://aa805ca87a7a84245ad5553a688cb953-352154593.us-east-1.elb.amazonaws.com"
+  default = "http://adcc3cf7e217a449da41b65abf3b6c76-1373874074.us-east-1.elb.amazonaws.com"
 }
 
 variable "order_uri_lb" {
   type    = string
-  default = "http://a73acb1e7965d4e1286288f0a3621d2b-457653939.us-east-1.elb.amazonaws.com"
+  default = "http://afacd6eebe0b346d98c936b0b461ebac-1571231343.us-east-1.elb.amazonaws.com"
 }
 
 variable "payment_uri_lb" {
   type    = string
-  default = "http://a89b05e80043745a89e878e92ab186a5-1759719953.us-east-1.elb.amazonaws.com"
+  default = "http://a45a20ee71a734df1a640486babf7387-602865925.us-east-1.elb.amazonaws.com"
 }
 
 variable "production_uri_lb" {
   type    = string
-  default = "http://a16d3f766dcc547f7b60dd6987a776e7-1733068445.us-east-1.elb.amazonaws.com"
+  default = "http://ac5de471384df4ef4a1875e8f27298be-692200852.us-east-1.elb.amazonaws.com"
 }
